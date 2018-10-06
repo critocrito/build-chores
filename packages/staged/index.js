f5699f847f5b1f46ece1162d4f02a31e99284ee6
Christo <christo@cryptodrunks.net>
2018-10-06 13:06:05 +0200

    feat: lint all package.json files when staging

--- packages/staged/index.js
+++ packages/staged/index.js
@@ -1,6 +1,6 @@
 module.exports = {
   "*.js": ["eslint --fix", "git add"],
-  "package.json": ["prettier-package-json --write", "git add"],
+  "**/package.json": ["prettier-package-json --write", "git add"],
   "*.json": ["prettier --write", "git add"],
   ".*rc": ["prettier --parser json --write", "git add"],
 };
